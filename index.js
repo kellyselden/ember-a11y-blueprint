6e35c1448a84abaa3642bfdd17e60a3f75bb75b5
Kelly Selden <kellyselden@gmail.com>
2019-12-04 09:14:27 +0000

    add blueprint

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  availableOptions: [
+    {
+      name: "lang",
+      type: String,
+      default: "en"
+    }
+  ],
+
+  locals(options) {
+    return {
+      lang: options.lang || this.availableOptions.find(o => o.name === 'lang').default
+    };
+  },
+
+  fileMapTokens(options) {
+    return {
+      __root__() {
+        return options.inAddon ? 'tests/dummy' : '';
+      }
+    };
+  }
 };
