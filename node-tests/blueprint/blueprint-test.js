ed1a6c5807ac9b7108f3d518f5fb46cc98454b6d
Kelly Selden <kellyselden@gmail.com>
2021-03-07 11:04:39 +0000

    ember-cli v3.23.0...v3.24.0

--- node-tests/blueprint/blueprint-test.js
+++ node-tests/blueprint/blueprint-test.js
@@ -4,7 +4,7 @@ const path = require('path');
 const {
   emberNew: _emberNew,
   emberInit: _emberInit,
-  setUpBlueprintMocha
+  setUpBlueprintMocha,
 } = require('ember-cli-update-test-helpers');
 const chai = require('chai');
 
@@ -12,77 +12,56 @@ chai.use(require('chai-fs'));
 
 const { expect } = chai;
 
-async function emberNew({
-  args = []
-}) {
+async function emberNew({ args = [] }) {
   return await _emberNew({
-    args: [
-      '-sn',
-      '-sg',
-      ...args
-    ]
+    args: ['-sn', '-sg', ...args],
   });
 }
 
-async function emberInit({
-  args = [],
-  cwd
-}) {
+async function emberInit({ args = [], cwd }) {
   return await _emberInit({
-    args: [
-      '-sn',
-      ...args
-    ],
-    cwd
+    args: ['-sn', ...args],
+    cwd,
   });
 }
 
-describe('blueprint', function() {
+describe('blueprint', function () {
   this.timeout(10 * 1000);
 
   setUpBlueprintMocha.call(this);
 
-  it('works with default options', async function() {
+  it('works with default options', async function () {
     let cwd = await emberNew({
-      args: [
-        '-b',
-        this.blueprintPath
-      ]
+      args: ['-b', this.blueprintPath],
     });
 
     expect(path.join(cwd, 'app/index.html'))
-      .to.be.a.file().with.contents.that.match(/<html lang="en">/);
+      .to.be.a.file()
+      .with.contents.that.match(/<html lang="en">/);
   });
 
-  it('works with custom options', async function() {
+  it('works with custom options', async function () {
     let cwd = await emberNew({
-      args: [
-        '-b',
-        this.blueprintPath,
-        '--lang=es'
-      ]
+      args: ['-b', this.blueprintPath, '--lang=es'],
     });
 
     expect(path.join(cwd, 'app/index.html'))
-      .to.be.a.file().with.contents.that.match(/<html lang="es">/);
+      .to.be.a.file()
+      .with.contents.that.match(/<html lang="es">/);
   });
 
-  it('works in addon', async function() {
+  it('works in addon', async function () {
     let cwd = await emberNew({
-      args: [
-        '-b=addon'
-      ]
+      args: ['-b=addon'],
     });
 
     await emberInit({
-      args: [
-        '-b',
-        this.blueprintPath
-      ],
-      cwd
+      args: ['-b', this.blueprintPath],
+      cwd,
     });
 
     expect(path.join(cwd, 'tests/dummy/app/index.html'))
-      .to.be.a.file().with.contents.that.match(/<html lang="en">/);
+      .to.be.a.file()
+      .with.contents.that.match(/<html lang="en">/);
   });
 });
